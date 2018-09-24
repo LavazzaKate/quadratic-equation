67853589537e381a606fd762b27e76a08ec24389
LavazzaKate <nebo10@mail.ru>
2018-09-24 15:12:58 +0300

    task4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+   equation = equation.replace(/\s+/g, '');
+  equation = equation.split('*x');
+  var a = +equation[0];
+  var c = +equation[2];
+ 
+  equation = equation[1];
+  equation = equation.split('^2');
+  var b = +equation[1];
+  
+  var x1 = -b/ 2 /a + Math.pow(Math.pow(b,2) - 4 * a * c , 0.5) / 2 / a;
+var x2 = -b/ 2 / a -Math.pow(Math.pow(b,2) - 4 * a * c ,0.5) / 2 / a;
+ 
+ var x1 = Math.round(x1);
+ var x2 = Math.round(x2);
+ 
+ if(x1 <= x2){
+   return([x1, x2]);
+  }else{
+    return([x2, x1]);
+  }
 }
